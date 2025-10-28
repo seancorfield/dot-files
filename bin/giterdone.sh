3d969b55ccb2a238cf36c506707db57cf23b83f1
Sean Corfield <sean@corfield.org>
2025-10-28 17:13:56 -0400

    add flexibility to bulk git script
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- bin/giterdone.sh
+++ bin/giterdone.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+# bulk git operations for my OSS and work projects:
+
+case "$1" in
+  branch)
+    ops="branch"
+    ;;
+  status)
+    ops="status"
+    ;;
+  *)
+    ops="remote prune origin"
+    ;;
+esac
+
 for r in .config/clojure oss/* oss/*/public workspace/*
 do
-  ( echo === $r === && cd $r && git pull -f && git remote prune origin )
+  ( echo === $r === && cd $r && git pull -f && git $ops )
 done
